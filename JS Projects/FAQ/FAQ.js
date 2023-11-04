1d5027aa1d7f8a1a8c551db05472ba257b88cc39
MukeshSivakumar <mukeshsivakumar97@gmail.com>
2023-11-04 08:18:18 +0530

    FAQ task added

---
+++ JS Projects/FAQ/FAQ.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let down_btnList = document.querySelectorAll(".down-icon");
+let answerList = document.querySelectorAll(".answer");
+let up_btnList = document.querySelectorAll(".up-icon");
+
+down_btnList.forEach(function(btn,index){
+
+    down_btnList[index].addEventListener("click",function(){
+        console.log(btn,index);
+
+        btn.classList.add("d-none");
+        up_btnList[index].classList.toggle("d-none");
+        answerList[index].classList.toggle("d-none");
+
+        answerList.forEach(function(item,indexAll){
+                
+             if(index != indexAll)
+             {
+                item.classList.add("d-none");
+                up_btnList[indexAll].classList.add("d-none");
+                down_btnList[indexAll].classList.remove("d-none");
+             }
+        });
+    });
+    up_btnList[index].addEventListener("click",function(){
+            console.log(up_btnList[index],index);
+            answerList[index].classList.toggle("d-none");
+            up_btnList[index].classList.toggle("d-none");
+            down_btnList[index].classList.toggle("d-none");
+        });
+});
+
+
+
